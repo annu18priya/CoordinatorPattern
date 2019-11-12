82a738d1f4d23aec38124f2f1bfbaccb15c3c381
annpriya <annapurna.priya@digitas.com>
2019-11-12 17:27:27 +0530

    xc_test_case : Test_Case_WIP

---
+++ CoordinatorDemoTests/SettingsTest.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsTest.swift
+//  CoordinatorDemoTests
+//
+//  Created by annpriya on 12/11/19.
+//  Copyright © 2019 annpriya. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+@testable import CoordinatorDemo
+
+class SettingsTest: XCTestCase {
+    
+    func testValidSettingResultData() {
+        
+    }
+}
